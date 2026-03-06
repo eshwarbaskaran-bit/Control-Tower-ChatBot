b847d6da94da4b0d8dce00cade80c67d3ac0761b
eshwarbaskaran-bit <eshwar.baskaran@clickpost.ai>
2026-03-06 11:13:58 +0000

    test

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_classic.chains.combine_documents import create_stuff_documents_chain
-from langchain_classic.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 # --- LOCAL TESTING MODE ---
